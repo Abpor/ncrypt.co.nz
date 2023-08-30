b92c41e6f155c690c1c13dbeb3b620321e2f7ea3
Rob <134886832+Abpor@users.noreply.github.com>
2023-08-30 23:43:50 +1200

    Upload of website from existing host
    Lift and shift from previous webhost.

---
+++ assets/config.js
@@ -0,0 +1,52 @@
+// cpanel - site_templates/vcard_legal/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "",
+        fax: "",
+        email: "herald@ncrypt.co.nz",
+        address: "",
+        social: [
+            
+            {
+                icon: 'facebook',
+                link: "https:\/\/www.facebook.com\/NcryptNZ\/"
+            },
+            
+            {
+                icon: 'linkedin',
+                link: "https:\/\/www.linkedin.com\/in\/rob-parkes-922994217\/"
+            },
+            
+            {
+                icon: 'discord',
+                link: "https:\/\/discord.gg\/UVcmCAcnXU"
+            },
+
+
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "Hey there, I am",
+            title: "Abpor (Rob)",
+            subtitle: "Game Dev, NZ Telco, Service Desk and Automation expertise",
+            type: 'vcard',
+            backgroundImage: "\/assets\/images\/legal\/rob_background.jpg",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            biography: "Check out my videos on Youtube following GameDev journey - https:\/\/www.youtube.com\/channel\/UCjWnSixN1yw8hiaTbTk5RYA \n\nJoin the Nearly Qualified podcast - we flesh out ideas about technology, video games, strategy and psychology : https:\/\/podcast.ncrypt.co.nz\n\nWe sell stuff, check it out here : https:\/\/store.ncrypt.co.nz\n\nIf you would like to buy me a coffee, please do so here : https:\/\/www.buymeacoffee.com/\Ncrypt/",
+            portraitImage: "\/assets\/images\/legal\/rob_portrait.jpg"
+        }
+    ]
+};
